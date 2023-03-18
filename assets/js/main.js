0192d1bde2056239d5e2bfe4ebd60e6f844296d9
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2023-03-18 08:56:23 -0300

    concluido

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,17 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+const sr =  ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 400,
+    // reset: true,
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, {delay: 700})
+sr.reveal(`.home__social, .home__scroll`, {delay: 900, origin: "bottom"})
+
